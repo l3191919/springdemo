b5b5978651a8a1f35ce2424f31a2e825d249ec0c
刘业钊 <lyz3191919@163.com>
2024-07-02 17:16:29 +0800

    thread.sleep

--- src/main/java/com/lyz/PaymentApplication.java
+++ src/main/java/com/lyz/PaymentApplication.java
@@ -1,5 +1,6 @@
 package com.lyz;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,10 +8,20 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.lyz.dao"})
+@Slf4j
 public class PaymentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentApplication.class, args);
+		int i = 0;
+		while (true){
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			log.info("启动JAVA:{}秒",i++);
+		}
 		//用来OOM测试
 //		List<user> list = new ArrayList<>();
 //		int i = 0;
